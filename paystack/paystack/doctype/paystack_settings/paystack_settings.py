ba98b166d4117a7054a75192b9e9cfc4e53d8e39
Isaac Larbi <isaac.larbi@access89.com>
2021-03-16 13:55:44 +0000

    sfda

--- paystack/paystack/doctype/paystack_settings/paystack_settings.py
+++ paystack/paystack/doctype/paystack_settings/paystack_settings.py
@@ -18,9 +18,9 @@ SUPPORTED_CURRENCIES = ['GHS']
 class PaystackSettings(Document):
 	supported_currencies = SUPPORTED_CURRENCIES
 
-	# def validate(self):
-	# 	if not self.flags.ignore_mandatory:
-	# 		self.validate_credentials()
+	def validate(self):
+		if not self.flags.ignore_mandatory:
+			self.validate_credentials()
 
 	def on_update(self):
 		name = 'Paystack-{0}'.format(self.gateway_name)
@@ -31,54 +31,54 @@ class PaystackSettings(Document):
 		)
 		call_hook_method('payment_gateway_enabled', gateway=name)
 
-	# def validate_credentials(self):
-	# 	api = paystakk.TransferControl(secret_key=self.secret_key, public_key=self.public_key)
-	# 	try:
-	# 		api.get_balance()
-	# 	except ConnectionError:
-	# 		frappe.throw('There was a connection problem. Please ensure that'
-	# 					 ' you have a working internet connection.')
+	def validate_credentials(self):
+		api = paystakk.TransferControl(secret_key=self.secret_key, public_key=self.public_key)
+		try:
+			api.get_balance()
+		except ConnectionError:
+			frappe.throw('There was a connection problem. Please ensure that'
+						 ' you have a working internet connection.')
 
-	# 	if not api.ctx.status:
-	# 		frappe.throw(api.ctx.message, title=_("Failed Credentials Validation"))
+		if not api.ctx.status:
+			frappe.throw(api.ctx.message, title=_("Failed Credentials Validation"))
 
-	# def validate_transaction_currency(self, currency):
-	# 	if currency not in self.supported_currencies:
-	# 		frappe.throw(
-	# 			_('{currency} is not supported by Paystack at the moment.').format(currency))
+	def validate_transaction_currency(self, currency):
+		if currency not in self.supported_currencies:
+			frappe.throw(
+				_('{currency} is not supported by Paystack at the moment.').format(currency))
 
-	# def get_payment_url(self, **kwargs):
-	# 	amount = kwargs.get('amount')
-	# 	description = kwargs.get('description')
-	# 	slug = kwargs.get('reference_docname')
-	# 	email = kwargs.get('payer_email')
-	# 	metadata = {
-	# 		'payment_request': kwargs.get('order_id'),
-	# 		'customer_name': kwargs.get('payer_name')
-	# 	}
+	def get_payment_url(self, **kwargs):
+		amount = kwargs.get('amount')
+		description = kwargs.get('description')
+		slug = kwargs.get('reference_docname')
+		email = kwargs.get('payer_email')
+		metadata = {
+			'payment_request': kwargs.get('order_id'),
+			'customer_name': kwargs.get('payer_name')
+		}
 
-	# 	secret_key = self.get_password(fieldname='secret_key', raise_exception=False)
+		secret_key = self.get_password(fieldname='secret_key', raise_exception=False)
 
-	# 	customer_api = paystakk.Customer(secret_key=secret_key, public_key=self.public_key)
+		customer_api = paystakk.Customer(secret_key=secret_key, public_key=self.public_key)
 
-	# 	customer_api.fetch_customer(email)
-	# 	if not customer_api.ctx.status:
-	# 		customer_api.create_customer(email=email)
+		customer_api.fetch_customer(email)
+		if not customer_api.ctx.status:
+			customer_api.create_customer(email=email)
 
-	# 	if not customer_api.ctx.status:
-	# 		frappe.throw(customer_api.ctx.message)
+		if not customer_api.ctx.status:
+			frappe.throw(customer_api.ctx.message)
 
-	# 	invoice_api = paystakk.Invoice(secret_key=secret_key, public_key=self.public_key)
+		invoice_api = paystakk.Invoice(secret_key=secret_key, public_key=self.public_key)
 
-	# 	identifier = hash('{0}{1}{2}'.format(amount, description, slug))
-	# 	invoice_api.create_invoice(customer=customer_api.customer_code,
-	# 							   amount=amount, due_date=nowdate(),
-	# 							   description=description,
-	# 							   invoice_number=identifier, metadata=metadata)
+		identifier = hash('{0}{1}{2}'.format(amount, description, slug))
+		invoice_api.create_invoice(customer=customer_api.customer_code,
+								   amount=amount, due_date=nowdate(),
+								   description=description,
+								   invoice_number=identifier, metadata=metadata)
 
-	# 	if not invoice_api.ctx.status:
-	# 		frappe.throw(invoice_api.ctx.message)
-	# 	else:
-	# 		payment_url = 'https://paystack.com/pay/{invoice_code}'.format(
-	# 			invoice_code=invoice_api.request_code)
-	# 		return payment_url
+		if not invoice_api.ctx.status:
+			frappe.throw(invoice_api.ctx.message)
+		else:
+			payment_url = 'https://paystack.com/pay/{invoice_code}'.format(
+				invoice_code=invoice_api.request_code)
+			return payment_url
