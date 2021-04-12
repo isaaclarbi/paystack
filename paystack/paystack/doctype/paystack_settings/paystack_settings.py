ad4a2cc96ec81cc3a2174b2de95af6d05af6ef7c
Isaac Larbi <isaac.larbi@access89.com>
2021-04-12 09:48:27 +0000

    Update paystack_settings.py

--- paystack/paystack/doctype/paystack_settings/paystack_settings.py
+++ paystack/paystack/doctype/paystack_settings/paystack_settings.py
@@ -71,21 +71,16 @@ class PaystackSettings(Document):
 		invoice_api = paystakk.Invoice(secret_key=secret_key, public_key=self.public_key)
 
 		identifier = hash('{0}{1}{2}'.format(amount, description, slug))
-		print("invoice number is:" + identifier)
-		print("amount:"+amount)
 		invoice_api.create_invoice(customer=customer_api.customer_code,
 								   amount=amount, due_date=nowdate(),
 								   description=description,
 								   currency='GHS',
 								   invoice_number=identifier, metadata=metadata)
 		
-		payment_url = 'https://paystack.com/pay/{invoice_code}'.format(
+		
+		if not invoice_api.ctx.status:
+			frappe.throw(invoice_api.ctx.message)
+		else:
+			payment_url = 'https://paystack.com/pay/{invoice_code}'.format(
 				invoice_code=invoice_api.request_code)
-		return payment_url
-
-		# if not invoice_api.ctx.status:
-		# 	frappe.throw(invoice_api.ctx.message)
-		# else:
-		# 	payment_url = 'https://paystack.com/pay/{invoice_code}'.format(
-		# 		invoice_code=invoice_api.request_code)
-		# 	return payment_url
+			return payment_url
